fb65a3139ce78ceb27f4c895dc586025d2d344ab
deechou <dchou1@gmail.com>
2024-01-18 13:56:06 -0500

    Question 2
    Signed-off-by: deechou <dchou1@gmail.com>

---
+++ chapter3/target/debug/chapter3.d
@@ -0,0 +1,1 @@
+C:\Users\echou\Documents\Projects\RustTutorials\rust-chapter-3\chapter3\target\debug\chapter3.exe: C:\Users\echou\Documents\Projects\RustTutorials\rust-chapter-3\chapter3\src\main.rs
---
+++ chapter3/target/debug/deps/chapter3.d
@@ -0,0 +1,5 @@
+C:\Users\echou\Documents\Projects\RustTutorials\rust-chapter-3\chapter3\target\debug\deps\chapter3.exe: src\main.rs
+
+C:\Users\echou\Documents\Projects\RustTutorials\rust-chapter-3\chapter3\target\debug\deps\chapter3.d: src\main.rs
+
+src\main.rs:
