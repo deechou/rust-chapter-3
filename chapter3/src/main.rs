671ca43efbb5dc56d407ddbec963e9f907b7cdd3
deechou <dchou1@gmail.com>
2024-01-26 17:34:41 -0500

    testing some loop stuff

--- chapter3/src/main.rs
+++ chapter3/src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
-    let number1 = 17;
-    let number2 = 121;
-    let number = if number1 > number2 { 5 } else { 6 };
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
 
-    println!("The value of number is: {number}");
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
 }
